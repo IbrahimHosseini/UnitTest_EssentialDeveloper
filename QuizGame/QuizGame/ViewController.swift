e7637a0d33bfe3321214a7257f5f28b864cc1c98
sehosseini@me.com <sehosseini@me.com>
2023-01-01 11:19:43 +0330

    init commit

---
+++ QuizGame/QuizGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Ibrahim on 1/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
